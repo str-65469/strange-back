e4bd0efa5a3d93c727900d9f2ee4f4c924cf77d8
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2021-10-15 19:22:56 +0400

    little refactor

--- src/http/user/user.controller.ts
+++ src/http/user/user.controller.ts
@@ -1,19 +1,11 @@
 import { UserSafeInterceptor } from './interceptor/user_safe.interceptor';
 import { UsersService } from './users.service';
 import { Controller, Get, UseGuards, UseInterceptors } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-import User from 'src/database/entity/user.entity';
 import { JwtAcessTokenAuthGuard } from '../auth/guards/jwt-access.guard';
 
 @Controller('user')
 export class UserController {
-  constructor(
-    //for now !!!
-    @InjectRepository(User)
-    private userRepo: Repository<User>,
-    private readonly userService: UsersService,
-  ) {}
+  constructor(private readonly userService: UsersService) {}
 
   @UseGuards(JwtAcessTokenAuthGuard)
   @UseInterceptors(UserSafeInterceptor)
