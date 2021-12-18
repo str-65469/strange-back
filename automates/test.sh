d1c173f42dedb51b975112b94844335927f96021
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2021-12-18 23:48:24 +0400

    env change via url done

---
+++ automates/test.sh
@@ -0,0 +1,1 @@
+echo hello world;yarn build;echo DONE !!!;
--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,23 @@
-import { MatchingSpams } from 'src/database/entity/matching_spams.entity';
-import { MatchedDuos } from 'src/database/entity/matched_duos.entity';
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import * as chProccess from 'child_process';
+import * as path from 'path';
+import * as fs from 'fs';
+import User from './database/entity/user.entity';
+import { Body, Controller, Get, NotFoundException, Post, UseGuards } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { In, Repository, Not, Raw, getRepository, MoreThan } from 'typeorm';
+import { Repository } from 'typeorm';
 import { AppService } from './app.service';
 import { ContactUsService } from './app/core/contact_us/contact_us.service';
 import { ContactUsDto } from './app/core/contact_us/dto/ContactUsDto';
-import { LolLeague } from './app/enum/lol_league.enum';
-import { LolServer } from './app/enum/lol_server.enum';
 import { MatchingLobby } from './database/entity/matching_lobby.entity';
-import User from './database/entity/user.entity';
 import { UserDetails } from './database/entity/user_details.entity';
 import { JwtAcessTokenAuthGuard } from './modules/auth/guards/jwt-access.guard';
-import { MatchedDuosNotifications } from './database/entity/matched_duos_notifications.entity';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly contactUsService: ContactUsService,
+
     // for test
     @InjectRepository(User) private readonly userRepo: Repository<User>,
     @InjectRepository(UserDetails) private readonly userDetalsRepo: Repository<UserDetails>,
@@ -36,88 +35,95 @@ export class AppController {
     return await this.contactUsService.contactUs(body);
   }
 
+  @Get('/replace')
+  async replace() {
+    const isExactMatch = (str, match) => {
+      const escapeRegExpMatch = match.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
+      return new RegExp(`\\b${escapeRegExpMatch}\\b`).test(str);
+    };
+
+    const automatePath = path.join(__dirname, '../../.env');
+    const file = fs.readFileSync(automatePath);
+    const textArr = file.toString().split('\n');
+
+    // search for keys and values
+    // const key1 = 'MUST_BE_REPLACED';
+    // const value1 = '123';
+
+    const keyvals = [
+      {
+        key: 'MUST_BE_REPLACED1',
+        value: 'RGAPI-0632c76c-256e-49be-a50a-f2bd54d6849a',
+      },
+      {
+        key: 'MUST_BE_REPLACED2',
+        value: 'RGAPI-0632c76c-256e-49be-a50a-f2bd54d6849a',
+      },
+      {
+        key: 'MUST_BE_REPLACED3',
+        value: 'RGAPI-0632c76c-256e-49be-a50a-f2bd54d6849a',
+      },
+    ];
+
+    keyvals.forEach((obj) => {
+      // replace with new value
+      let key1Index = textArr.findIndex((el) => isExactMatch(el, obj.key));
+
+      if (key1Index === -1) {
+        throw new NotFoundException();
+      }
+
+      let key1Value = textArr[key1Index]; // key and value joined e.g. key=value
+      let key1Arr = key1Value.split('=');
+      key1Arr[1] = obj.value;
+      key1Value = key1Arr.join('='); // new key and value joined e.g. key=new_value
+      textArr[key1Index] = key1Value;
+    });
+
+    const finalFileString = textArr.join('\n');
+
+    try {
+      fs.writeFileSync(automatePath, finalFileString, 'utf8');
+
+      return {
+        msg: 'replaced',
+      };
+    } catch (error) {
+      console.log(error);
+      throw new NotFoundException();
+    }
+  }
+
   @Get('/test')
   async test() {
-    return await getRepository(MatchedDuos)
-      .find({
-        where: { user: 1 },
-        relations: ['matchedUser'],
-      })
-      .then((matcheds) => matcheds.map((m) => m.matchedUser));
+    const automatePath = path.join(__dirname, '../../', '/automates/test.sh');
 
-    return this.userDetalsRepo.findOne(1);
-    return await this.userDetalsRepo.findOne({
-      where: {
-        user: Not(In([356, 357, 358, 359])),
-        league: In([LolLeague.BRONZE, LolLeague.SILVER]),
-        server: LolServer.EU_NORDIC_WEST,
-      },
-
-      order: { id: 'ASC' },
-      relations: ['user'],
-    });
-    const prevId = 400;
-
-    return await this.userDetalsRepo.findOne({
-      where: {
-        user: Raw((alias) => `${alias} > :prevId AND ${alias} NOT IN (:...Ids)`, { prevId, Ids: [356, 357, 358, 359] }),
-        league: In([LolLeague.BRONZE, LolLeague.SILVER]),
-        server: LolServer.EU_NORDIC_WEST,
-      },
-      order: { id: 'ASC' },
-      relations: ['user'],
+    chProccess.exec(`sh ${automatePath}`, (error, stdout, stderr) => {
+      console.log(stdout);
+      console.log(stderr);
+      if (error !== null) {
+        console.log(`exec error: ${error}`);
+      }
     });
 
-    const user = await this.userRepo.findOne(1);
-    const mines = await this.lobbyRepo.find({ where: { user: user }, relations: ['user', 'likedUser'] });
-    const oponentIds = mines.map((e) => e.likedUser.id);
-
-    if (mines && mines.length) {
-      //   return oponents;
-      return await this.lobbyRepo
-        .find({
-          where: {
-            user: Not(In(oponentIds)),
-            // likedUser: user,
-          },
-
-          relations: ['user'],
-        })
-        .then((lobbies) => lobbies.map((l) => l.user));
-    }
-
-    // return [];
-    // const userAll = this.userRepo.findOne(1, {
-    //   relations: ['spams', 'details'],
-    // });
-    // return userAll;
-    // return { message: 'hello' };
+    return `sh ${automatePath}`;
   }
 
-  @Get('/cleanall')
-  async cleanSpams() {
-    if (process.env.NODE_ENV === 'development') {
-      getRepository(MatchingSpams)
-        .createQueryBuilder()
-        .update()
-        .set({ accept_list: [], decline_list: [], remove_list: [], matched_list: [] })
-        .execute();
+  //   @Get('/cleanall')
+  //   async cleanSpams() {
+  //     if (process.env.NODE_ENV === 'development') {
+  //       getRepository(MatchingSpams)
+  //         .createQueryBuilder()
+  //         .update()
+  //         .set({ accept_list: [], decline_list: [], remove_list: [], matched_list: [] })
+  //         .execute();
 
-      getRepository(MatchedDuos).createQueryBuilder().delete().where('id > 0').execute();
-      getRepository(MatchedDuosNotifications).createQueryBuilder().delete().where('id > 0').execute();
-      getRepository(MatchingLobby).createQueryBuilder().delete().where('id > 0').execute();
-      return 'cleaned';
-    }
+  //       getRepository(MatchedDuos).createQueryBuilder().delete().where('id > 0').execute();
+  //       getRepository(MatchedDuosNotifications).createQueryBuilder().delete().where('id > 0').execute();
+  //       getRepository(MatchingLobby).createQueryBuilder().delete().where('id > 0').execute();
+  //       return 'cleaned';
+  //     }
 
-    return 'not so fast';
-  }
-
-  @Get('/relations')
-  async getRelations() {
-    const users = await this.userRepo.find({
-      relations: ['details'],
-    });
-
-    return users;
-  }
+  //     return 'not so fast';
+  //   }
 }
